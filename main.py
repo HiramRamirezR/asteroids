72bed71200942f1feb61cce2d9dce04c60885bcb
HiramRamirezR <hiram.j.ramirez@gmail.com>
2025-08-06 21:44:50 -0600

    space ship flying and asteroids randomly created

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -7,6 +10,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
+
+    # Game groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+    # Initialize objects
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+    asteroid_field = AsteroidField()
 
     # Game loop
     while True:
@@ -14,6 +34,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        dt = time.tick(60) / 1000
+
+        for draw in drawable:
+            draw.draw(screen)
+
+        updatable.update(dt)
+
         pygame.display.flip()
 
 if __name__ == "__main__":
